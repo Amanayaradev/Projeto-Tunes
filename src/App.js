ee1e41197010978f46939845da0c7ec0a77bea35
Amanayara Slot <amanayaradev@gmail.com>
2023-04-13 15:07:02 -0300

    requisito um

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Login from './componentes/Login';
+import Search from './componentes/Search';
+import Album from './componentes/Album';
+import Favorites from './componentes/Favorites';
+import Profile from './componentes/Profile';
+import NotFound from './componentes/NotFound';
+import ProfileEdit from './componentes/ProfileEdit';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="*" component={ NotFound } />
+        </Switch>
+      </div>
+    );
   }
 }
 
