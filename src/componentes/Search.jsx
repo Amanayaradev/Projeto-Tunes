b3b257ce3bfc8fa845eb3610ac0ccbad512808d0
Amanayara Slot <amanayaradev@gmail.com>
2023-04-13 22:24:54 -0300

    requisito cinco

--- src/componentes/Search.jsx
+++ src/componentes/Search.jsx
@@ -2,11 +2,40 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 class Search extends Component {
+  state = {
+    searchInput: '',
+  };
+
+  searchInput = ({ target }) => {
+    this.setState({
+      searchInput: target.value,
+    });
+  };
+
   render() {
+    const valueMin = 2;
+    const { searchInput } = this.state;
+    const disabled = searchInput.length < valueMin;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search</p>
+        <form>
+          <label htmlFor="search">
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              onChange={ this.searchInput }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            disabled={ disabled }
+            onClick={ this.procuraBtn }
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }
